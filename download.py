0e74273121dda0c5db0c99badf1d2a7a8c84078e
Senacchi <senatang124@gmail.com>
2024-05-26 12:31:41 +0800

    Fix issue of download photo

--- download.py
+++ download.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 from bs4 import BeautifulSoup
 from markdownify import markdownify as md
+from urllib.parse import urljoin
 
 def start_scraping_after_manual_login():
     driver = webdriver.Chrome()
@@ -38,7 +39,7 @@ def start_scraping_after_manual_login():
             link = links[i]
             href = link.get_attribute('href')
             driver.get(href)
-            time.sleep(1)
+            time.sleep(3)
 
             content_data = extract_content(driver)
             if content_data:
@@ -79,12 +80,7 @@ def extract_content(driver):
 
 def save_content(name, date, title, html_content, full_page_html):
     member_folder = os.path.join(os.getcwd(), name)
-    try:
-        date_folder = datetime.strptime(date, "%Y.%m.%d %H:%M").strftime("%Y-%m-%d_%H-%M-%S")
-    except ValueError as e:
-        print(f"Error parsing date: {e}")
-        return  
-
+    date_folder = datetime.strptime(date, "%Y.%m.%d %H:%M").strftime("%Y-%m-%d_%H-%M")
     path = os.path.join(member_folder, date_folder)
     
     if not os.path.exists(member_folder):
@@ -95,19 +91,29 @@ def save_content(name, date, title, html_content, full_page_html):
     file_path = os.path.join(path, 'content.md')
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write("# " + title + "\n\n" + md(html_content)) 
-    
+
+    base_url = "https://takanekofc.com/"  
+
     soup = BeautifulSoup(full_page_html, 'html.parser')
     for img in soup.find_all('img'):
         img_url = img.get('src')
-        if img_url and img_url.startswith('http'):
+        if not img_url.startswith(('http', 'https')):
+            img_url = urljoin(base_url, img_url)  
+
+        try:
             img_response = requests.get(img_url, stream=True)
-            img_name = img_url.split('/')[-1]
-            img_path = os.path.join(path, img_name)
-            with open(img_path, 'wb') as img_file:
-                for chunk in img_response.iter_content(1024):
-                    img_file.write(chunk)
+            if img_response.status_code == 200:
+                img_name = img_url.split('/')[-1]
+                img_path = os.path.join(path, img_name)
+                with open(img_path, 'wb') as img_file:
+                    for chunk in img_response.iter_content(1024):
+                        img_file.write(chunk)
+            else:
+                print(f"Failed to download {img_url}: Server responded with status code {img_response.status_code}")
+        except Exception as e:
+            print(f"Error downloading {img_url}: {str(e)}")
+
 
     print(f"Content and images saved for {name} on {date}")
-
 
 start_scraping_after_manual_login()
